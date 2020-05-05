c8b694ed321ff5858795ebb27116d8cc5333cab0
Rishabh Agrawal <therishabhagrawal@gmail.com>
2020-05-05 17:34:58 +0530

    basic integration of socket.io with frontend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, './../public');
 const express = require('express');
-
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
 
 var app = express();
+let server = http.createServer(app)
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log(`Server is up on port : 3000`);
+io.on('connection', (socket) => {
+    console.log('A New user just connected');
+    socket.on('disconnect', () => {
+        console.log('User has been disconnected');
+    })
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port : ${port}`);
 })
