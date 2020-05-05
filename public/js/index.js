eff097b9201b5272dc824e38c137b21fca019e93
Rishabh Agrawal <therishabhagrawal@gmail.com>
2020-05-05 21:35:07 +0530

    added custom braodcasting events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+let socket = io();
+
+socket.on('connect', function () {
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+        from: "Rishabh",
+        text: "Hello Everyone"
+    })
+})
+
+socket.on('disconnect', function () {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New Message', message)
+})
+
--- server/server.js
+++ server/server.js
@@ -13,6 +13,46 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('A New user just connected');
+
+    socket.emit('newMessage', {
+        from : "Admin",
+        text : "Welcome to archchat Chat box",
+        createdAt : new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from : 'Admin',
+        text : "New User Joined",
+        createdAt : new Date().getTime()
+    })
+
+    
+    // socket.emit('newMessage', {
+    //     name : "Lorem",
+    //     text : "Hello Lorem"
+    // });
+
+    // listen event
+    socket.on('createMessage', (message) => {
+        console.log('Create Message', message);
+
+        // == emit to everyone (include self)
+        // io.emit('newMessage', {
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // });
+
+        // == emit to everyone (expect self)
+        // socket.broadcast.emit('newMessage', {
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // });
+
+    });
+
+
     socket.on('disconnect', () => {
         console.log('User has been disconnected');
     })
